1e6277c920bc009d48f3073cbe188573a6fd8a6a
RzayevGara <qara109@gmail.com>
2022-01-08 18:08:40 +0400

    sale and under section

--- js/click.js
+++ js/click.js
@@ -44,21 +44,80 @@ s=document.getElementById("carousel-swipe")
 
     function firstbtn(){
         s.style.left="0%"
-
+        document.getElementById("active-first").style.color="rgba(245, 245, 245)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245, 0.384)"
     }
     function secondbtn(){
         s.style.left="-100%"
+        document.getElementById("active-first").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245, 0.384)"
     }
     function thirdbtn(){
         s.style.left="-200%"
+        document.getElementById("active-first").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245, 0.384)"
     }
     function fourthbtn(){
         s.style.left="-300%"
-
+        document.getElementById("active-first").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245, 0.384)"
     }
     function fifthbtn(){
         s.style.left="-400%"
+        document.getElementById("active-first").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245, 0.384)"
     }
     function sixthbtn(){
         s.style.left="-500%"
+        document.getElementById("active-first").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-second").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-third").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fourth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-fifth").style.color="rgba(245, 245, 245, 0.384)"
+        document.getElementById("active-sixth").style.color="rgba(245, 245, 245)"
     }
+
+    if(s.style.left="0%"){
+        document.getElementById("active-first").style.color="rgba(245, 245, 245)"
+    }
+
+    // var swiper = new Swiper(".mySwiper", {
+    //     slidesPerView: 2,
+    //     spaceBetween: 30,
+    //     pagination: {
+    //       el: ".swiper-pagination",
+    //       clickable: true,
+    //       renderBullet: function (index, className) {
+    //         return '<span class="' + className + '">' + "</span>";
+    //       },
+    //     },
+    //   });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
